cd85d96241b18f5211a0bd6e58625b9a946fc1f6
arielchang0419 <arielbosh@gmail.com>
2018-06-19 19:32:15 +0800

    這次是測試 我加了login的介面！！

---
+++ app/src/main/java/com/example/eating/wearight/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.eating.wearight;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+
+
+
+
+}
